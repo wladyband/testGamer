8ad67e38525a5673466218903d1c3c03a513f654
wladimir <tecnico100@gmail.com>
2018-12-26 16:45:01 -0200

    restruturação do main

--- src/main/java/com/test/wladimir/gamergourmet/main/GamerGourmet.java
+++ src/main/java/com/test/wladimir/gamergourmet/main/GamerGourmet.java
@@ -9,6 +9,18 @@ import javax.swing.JLabel;
 
 public class GamerGourmet {
 
+    private static final Integer width = 280;
+    private static final Integer height = 140;
+    private static final Integer right_square = 40;
+    private static final Integer height_square = 12;
+    private static final Integer padding_text = 180;
+    private static final Integer padding_height = 20;
+
+    private static final Integer right_button = 90;
+    private static final Integer height_button = 40;
+    private static final Integer padding_button = 55;
+    private static final Integer padding_height_button = 30;
+
     private static final String REMEMBER_ONE_PLATE = "Pense em um prato que gosta.";
 
     public static void main(String[] args) {
@@ -17,6 +29,7 @@ public class GamerGourmet {
     }
 
     private void start() {
+
         final Beginning beginning = new Beginning("massa");
         beginning.setInitialOptions("Lazanha", "Bolo de Chocolate");
 
@@ -24,16 +37,26 @@ public class GamerGourmet {
         JLabel jlabel = new JLabel(REMEMBER_ONE_PLATE);
         JButton jbuttonOk = new JButton("OK");
 
-        jframe.setSize(300, 200);
+        jframe.setSize(this.width, this.height);
         jframe.setLocationRelativeTo(null);
         jframe.setVisible(true);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.setLayout(null);
 
-        jlabel.setBounds(50, 40, 200, 30);
+        jlabel.setBounds(
+                this.right_square,
+                this.height_square,
+                this.padding_text,
+                this.padding_height
+        );
         jframe.add(jlabel);
 
-        jbuttonOk.setBounds(90, 100, 100, 30);
+        jbuttonOk.setBounds(
+                this.right_button,
+                this.height_button,
+                this.padding_button,
+                this.padding_height_button
+        );
         jframe.add(jbuttonOk);
 
         jbuttonOk.addActionListener(new ActionListener() {
